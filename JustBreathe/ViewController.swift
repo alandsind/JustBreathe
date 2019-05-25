4a7b7aea1c02587a109fa7ae6674b91b4dbbac8e
Aland Sinduartha <alandsind@gmail.com>
2019-05-26 00:25:22 +0700

    Add music but doesnt work

--- JustBreathe/ViewController.swift
+++ JustBreathe/ViewController.swift
@@ -16,8 +16,8 @@ class ViewController: UIViewController {
     var bgColors: [UIColor] = [#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1), #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)]
     var index: Int = 0
     var viewForGradient = UIView()
-    var songPlayer = AVAudioFile()
     var timer = Timer()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +25,7 @@ class ViewController: UIViewController {
         index = 0
         setupTimer()
         
+        //swiping gradient background color animation
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [UIColor(white: 1, alpha: 0).cgColor, UIColor(white: 1, alpha: 0.7).cgColor, UIColor(white: 1, alpha: 0.72).cgColor, UIColor(white: 1, alpha: 0.7).cgColor, UIColor(white: 1, alpha: 0).cgColor]
         gradientLayer.locations = [0, 0.48, 0.5, 0.52, 1]
@@ -36,6 +37,7 @@ class ViewController: UIViewController {
         self.view.addSubview(viewForGradient)
     
     }
+    //pulsating background animation
     func setupTimer() {
         timer = Timer.scheduledTimer(timeInterval: 4, target: self, selector: #selector(handleColorChange), userInfo: nil, repeats: true)
     }
@@ -56,6 +58,7 @@ class ViewController: UIViewController {
             
         })
         
+        //pulsating inhale exhale text animation & swiping background position animation
         UIView.animate(withDuration: 4, delay: 0, options: .curveEaseOut, animations: {
             self.breathIndicator.text = "Exhale"
             self.breathIndicator.alpha = 0
@@ -71,6 +74,31 @@ class ViewController: UIViewController {
                 self.viewForGradient.frame = CGRect(x: 0, y: -448, width: self.viewForGradient.frame.width, height: self.viewForGradient.frame.height)
             })
         }
+        
+        //add music but why doesnt work
+         
+        class MusicPlayer {
+            static let shared = MusicPlayer()
+            var audioPlayer: AVAudioPlayer?
+            
+            func startBackgroundMusic() {
+                if let bundle = Bundle.main.path(forResource: "BGM", ofType: "mp3") {
+                    let backgroundMusic = NSURL(fileURLWithPath: bundle)
+                    do {
+                        audioPlayer = try AVAudioPlayer(contentsOf:backgroundMusic as URL)
+                        guard let audioPlayer = audioPlayer else { return }
+                        audioPlayer.numberOfLoops = -1
+                        audioPlayer.prepareToPlay()
+                        audioPlayer.play()
+                    } catch {
+                        print(error)
+                    }
+                }
+            }
+        }
+        
+        MusicPlayer.shared.startBackgroundMusic()
+        
     }
 }
 
